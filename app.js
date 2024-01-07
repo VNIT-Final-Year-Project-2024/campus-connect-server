8f92284c4d22486428c72589aad50b025e1f033e
Mayur Shegokar <mayurshegokar.exe@gmail.com>
2024-01-07 15:44:22 +0530

    enabled CORS

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var logger = require('morgan');
 
 const app = express();
 
-// setting up middlewares
+// enable CORS for all routes
+app.use(cors());
+
+// set up middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

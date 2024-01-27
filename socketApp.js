19d3308044b13c73b15f230529fc697a5da18d47
Mayur Shegokar <mayurshegokar.exe@gmail.com>
2024-01-27 14:14:12 +0530

    used rooms to manage clients in Socket.IO

--- socketApp.js
+++ socketApp.js
@@ -4,18 +4,20 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
-// object to store Socket.IO connections
-const clients = {};
+// server defined at module-level to pass into other functions
+let io;
 
 // function to initialize Socket.IO server
 const initSocketIOServer = (server) => {
-    const io = new Server(server);
+    io = new Server(server);
 
     // Socket.IO auth middleware
     io.use((socket, next) => {
         if (socket.handshake.headers && socket.handshake.headers.authorization) {
-            // Extract the token from the 'Authorization' header
-            const token = socket.handshake.headers.authorization
+
+            // extract the token from the 'Authorization' header
+            const token = socket.handshake.headers.authorization;
+
             jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
                 if (err) return next(new Error('Auth token invalid'));
                 socket.user = decoded.user;
@@ -24,19 +26,15 @@ const initSocketIOServer = (server) => {
         } else {
             next(new Error('Auth token missing'));
         }
-    })
+    });
 
     io.on('connection', (socket) => {
         const { groupId } = socket.handshake.query;
-
         socket.join(groupId);
-        clients[groupId] = clients[groupId] || [];
-        clients[groupId].push(socket);
 
         console.log(`User: ${socket.user.name} connected to group: ${groupId}`);
 
         socket.on('disconnect', () => {
-            clients[groupId] = clients[groupId].filter(client => client !== socket);
             console.log(`User: ${socket.user.name} disconnected from group: ${groupId}`);
         });
     });
@@ -46,18 +44,12 @@ const initSocketIOServer = (server) => {
 const run = (port, callback) => {
     const server = http.createServer();
     initSocketIOServer(server);
-
-    // Start the server and listen on the specified port
     server.listen(port, callback);
 };
 
 // function to send message to all clients in a group
 const sendUpdateToGroup = (groupId, message) => {
-    if (clients[groupId]) {
-        clients[groupId].forEach(socket => {
-            socket.emit('message', { type: 'message', message });
-        });
-    }
+    io.to(groupId).emit('message', { type: 'message', message });
     console.log(`Update to ${groupId} sent to all connected clients`);
 };
 

a0cfa669a327cc426d66c51b85d936c5f740c239
Mayur Shegokar <mayurshegokar.exe@gmail.com>
2024-03-07 19:43:51 +0530

    readibility for group and message models

--- models/groups.js
+++ models/groups.js
@@ -1,13 +1,36 @@
 const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
-  name: { type: String, maxlength: 30 },
-  is_chatroom: { type: Boolean },
-  members: [{ id: Number, name: String }],
-  description: { type: String },
-  recent_activity: { timestamp: { type: Date }, message: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' } },
-  avatar: { type: String },
-  created_at: { type: Date, default: Date.now },
+  name: { 
+    type: String, 
+    maxlength: 30 
+  },
+  is_chatroom: { 
+    type: Boolean 
+  },
+  members: [{ 
+    id: Number, 
+    name: String 
+  }],
+  description: { 
+    type: String 
+  },
+  recent_activity: { 
+    timestamp: { 
+      type: Date 
+    }, 
+    message: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'Message' 
+    } 
+  },
+  avatar: { 
+    type: String 
+  },
+  created_at: { 
+    type: Date, 
+    default: Date.now 
+  },
 });
 
 const Group = mongoose.model('Group', groupSchema);

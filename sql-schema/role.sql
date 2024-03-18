dd183d5da58c504959c5a484b97ac6f4f80ab4b2
Mayur Shegokar <mayurshegokar.exe@gmail.com>
2024-03-18 17:31:59 +0530

    added send message permission for roles

--- sql-schema/role.sql
+++ sql-schema/role.sql
@@ -10,24 +10,25 @@ CREATE TABLE IF NOT EXISTS role (
 -- Inserting pre-defined roles
 INSERT INTO role (name, permissions, description)
 VALUES 
-    ('Professor', 127, 'The professor incharge of the club'),
-    ('President', 63, 'The president of the club'),
-    ('Treasurer', 31, 'The treasurer of the club'),
-    ('Member', 15, 'A regular member of the club'),
-    ('Volunteer', 0, 'A volunteer for the club');
+    ('Professor', 255, 'The professor incharge of the club'),
+    ('President', 127, 'The president of the club'),
+    ('Treasurer', 63, 'The treasurer of the club'),
+    ('Member', 31, 'A regular member of the club'),
+    ('Volunteer', 1, 'A volunteer for the club');
 
 -- PERMISSIONS
--- 1 - Update Group Details
--- 2 - Add Group Member
--- 4 - Create Group
--- 8 - Delete Group
--- 16 - Update Club Details
--- 32 - Add Club Member
--- 64 - Delete Club
+-- 1 - Send Message (inside group)
+-- 2 - Update Group Details
+-- 4 - Add Group Member
+-- 8 - Create Group
+-- 16 - Delete Group
+-- 32 - Update Club Details
+-- 64 - Add Club Member
+-- 128 - Delete Club
 
 -- PERMISSION STRINGS
--- Professor - 1111111
--- President - 0111111
--- Treasurer - 0011111
--- Member    - 0001111
--- Volunteer - 0000000
+-- Professor - 11111111
+-- President - 01111111
+-- Treasurer - 00111111
+-- Member    - 00011111
+-- Volunteer - 00000001

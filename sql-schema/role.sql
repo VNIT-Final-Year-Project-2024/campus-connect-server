bb2b67f42a7002ac4b62ba2da11d8de358ca199f
Mayur Shegokar <mayurshegokar.exe@gmail.com>
2024-03-18 17:26:56 +0530

    updated permissions for roles

--- sql-schema/role.sql
+++ sql-schema/role.sql
@@ -10,19 +10,24 @@ CREATE TABLE IF NOT EXISTS role (
 -- Inserting pre-defined roles
 INSERT INTO role (name, permissions, description)
 VALUES 
-    ('Professor', 511, 'The professor incharge of the club'),
-    ('President', 127, 'The president of the club'),
+    ('Professor', 127, 'The professor incharge of the club'),
+    ('President', 63, 'The president of the club'),
     ('Treasurer', 31, 'The treasurer of the club'),
     ('Member', 15, 'A regular member of the club'),
-    ('Volunteer', 1, 'A volunteer for the club');
+    ('Volunteer', 0, 'A volunteer for the club');
 
 -- PERMISSIONS
--- 1 - View Messages
--- 2 - Update Group Details
--- 4 - Add Group Member
--- 8 - Create Group
--- 16 - Delete Group
--- 32 - Update Club Details
--- 64 - Add Club Member
--- 128 - Create Club
--- 256 - Delete Club
+-- 1 - Update Group Details
+-- 2 - Add Group Member
+-- 4 - Create Group
+-- 8 - Delete Group
+-- 16 - Update Club Details
+-- 32 - Add Club Member
+-- 64 - Delete Club
+
+-- PERMISSION STRINGS
+-- Professor - 1111111
+-- President - 0111111
+-- Treasurer - 0011111
+-- Member    - 0001111
+-- Volunteer - 0000000
